83a6119e7d45e636d8c08d79268bf8e6dfe32d08
sql-fabrik <git4schulung@sql-fabrik.de>
2024-11-29 15:26:56 +0100

    'copy_EA.. '  added

---
+++ copy_EA_files_2targetFolder.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\ALGOWAY TRADING BOT.ex4"                "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\AALGOWAY TRADING BOT.ex4"               
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\ALPHA ROBOT V1.ex4"                     "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\AALPHA ROBOT V1.ex4"                    
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\CASHFLOW-SYSTEM-PRO.ex4"                "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ACASHFLOW-SYSTEM-PRO.ex4"               
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\NEXT_LEVEL_AI.ex4"                      "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ANEXT_LEVEL_AI.ex4"                     
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\PROFIT GENERATOR.ex4"                   "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\APROFIT GENERATOR.ex4"                  
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RBH SCALPER EA.ex4"                     "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARBH SCALPER EA.ex4"                    
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RH EA PRO.ex4"                          "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARH EA PRO.ex4"                         
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RH EA.ex4"                              "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARH EA.ex4"                             
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RobinH_EA [Trading Butler Robot].ex4"   "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARobinH_EA [Trading Butler Robot].ex4"  
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RobotFX.ex4"                            "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARobotFX.ex4"                           
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\RobsHoodEA.ex4"                         "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ARobsHoodEA.ex4"                        
+copy  "C:\software\MetaTrader_EA_RobinH_2024nov\MQL4\Experts\TheRobinsHoodEA #1.ex4"                 "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ATheRobinsHoodEA #1.ex4"                
+
+
+pause
+
+
+REM  --  93A3414A71EECFA76DD60A3D127E44BA  --  fxflat
+REM  --  F1BBCAACDA8825381C125EAF07296C41  --  vantageInternational
+
+
