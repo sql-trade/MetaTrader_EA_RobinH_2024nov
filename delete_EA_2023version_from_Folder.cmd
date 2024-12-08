706fdda188dc3dcba6dc3fea9fa2a87796efb849
sql-trade <97407735+sql-trade@users.noreply.github.com>
2024-12-08 19:00:30 +0100

    Add files via upload

---
+++ delete_EA_2023version_from_Folder.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ALGOWAY TRADING BOT.ex4"                 
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\ALPHA ROBOT V1.ex4"                      
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\CASHFLOW-SYSTEM-PRO.ex4"                 
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\FX ROBOT EA By Rob - Buy.ex4"            
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\FX ROBOT EA By Rob - Sell.ex4"           
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\FX ROBOT EA c By Rob - Buy.ex4"          
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\FX ROBOT EA c By Rob - Sell.ex4"         
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\Master_Profit_Generator.ex4"             
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\NEXT_LEVEL_AI.ex4"                       
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\PROFIT GENERATOR.ex4"                    
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\PROFIT_GENERATOR_PRO.ex4"                
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RBH SCALPER EA.ex4"                      
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RH EA PRO.ex4"                           
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RH EA.ex4"                               
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RobinHoodEA.ex4"                         
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RobinH_EA [Trading Butler Robot].ex4"    
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RobinsHoodEA #1.ex4"                     
+del  "C:\Users\<...username...>\AppData\Roaming\MetaQuotes\Terminal\<...uid...>\MQL4\Experts\RobotFX.ex4"                             
+
+
+pause
+
+
+
+
+REM  --  93A3414A71EECFA76DD60A3D127E44BA  --  fxflat
+REM  --  F1BBCAACDA8825381C125EAF07296C41  --  vantageInternational
